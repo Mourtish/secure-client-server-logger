8d30e8d31f5fe406da981aed5a23eecb8581541d
Mourtalla Diop <mourtalladiop127@gmail.com>
2026-05-28 04:26:15 +0000

    Added API key authentication and input validation

--- client/client.py
+++ client/client.py
@@ -2,10 +2,14 @@ import requests
 
 url = "http://localhost:5000/log"
 
+headers = {
+    "X-API-KEY": "supersecretkey"
+}
+
 data = {
     "message": "Unauthorized login attempt"
 }
 
-response = requests.post(url, json=data)
+response = requests.post(url, json=data, headers=headers)
 
 print(response.json())
